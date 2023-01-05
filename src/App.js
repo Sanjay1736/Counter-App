d28803356a20b3a85aca0796ed3d6448abd94f18
Gudipudi Sanjay <sanjaygudipudi2@gmail.com>
2023-01-05 07:49:00 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./styles.css";
-
+// import "./styles.css";
+import React, { useState } from "react";
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>
+        Counter: <b>{count}</b>
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button disabled={counter < -1} onClick={() => setCounter(counter - 1)}>
+        Decrement
+      </button>
     </div>
   );
 }
